0628db4e41f11730093c4c950034aa649fa2fca0
Jonnier197 <Jonnier.cadena01@unicatolica.edu.co>
2025-02-23 14:20:52 -0500

    InsertionSort.py

--- InsertionSort.py
+++ InsertionSort.py
@@ -2,12 +2,12 @@ def insertion_sort(A):
     Longitud = len(A)
     i = 1
     while i < Longitud:
-        current = A[i]
+        pos = A[i]
         j = i - 1
-        while j >= 0 and A[j] > current:
+        while j >= 0 and A[j] > pos:
             A[j + 1] = A[j]
             j = j - 1
-        A[j + 1] = current
+        A[j + 1] = pos
         i = i + 1
 
 #Array a ordenar
